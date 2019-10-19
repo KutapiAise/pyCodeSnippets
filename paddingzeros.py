4d0ad37aaf85fd1a59f5dba04bc0201d37e19d7b
Aishwarya A.S <ashkash.aishwarya@gmail.com>
2019-10-19 12:10:31 +0400

    Update paddingzeros.py

--- paddingzeros.py
+++ paddingzeros.py
@@ -1,11 +1,11 @@
-In Python 2 you can do:
+#In Python 2 you can do:
 
 print "%02d" % (1,)
-Basically % is like printf or sprintf.
+#Basically % is like printf or sprintf.
 
-For Python 3.+ the same behavior can be achieved with:
+#For Python 3.+ the same behavior can be achieved with:
 
 print("{:02d}".format(1))
-For Python 3.6+ the same behavior can be achieved with f-strings:
+#For Python 3.6+ the same behavior can be achieved with f-strings:
 
 print(f"{1:02d}")
